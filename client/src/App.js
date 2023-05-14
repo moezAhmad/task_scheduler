17ac6772c5bb0e48ee303384b7ed517d35c6a5b2
Muhammad Talha <muhammadtalha@Defo-MacBook-Air.local>
2023-05-14 17:07:30 +0500

    Styling done in the style.css file under client/src/style.css, index.css removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
 import axios from "axios";
+import './style.css';
 
 function App() {
   const [code, setCode] = useState("// Write your code here...");
@@ -34,30 +35,43 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Distributed Task Scheduler</h1>
-      <label>
-          Language:
-          <select value={language} onChange={(e) => setLanguage(e.target.value)}>
-            <option value="javascript">JavaScript</option>
-            <option value="python">Python</option>
-            {/* Add other languages here */}
-          </select>
+      
+      <section id="header">
+        <h1>Distributed Task Scheduler</h1>
+        <label class="select-button">
+            {/* Language  */}
+            <select  class="btn btn-info btn-height" value={language} onChange={(e) => setLanguage(e.target.value)}>
+              <option value="javascript">JavaScript</option>
+              <option value="python">Python</option>
+              {/* Add other languages here */}
+            </select>
+            <button className="btn btn-dark btn-height" onClick={handleSubmit}>Submit</button>
         </label>
-      <Editor
-        height="70vh"
-        defaultLanguage={language}
-        value={code}
-        onChange={handleEditorChange}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-      <div className="result-window">
-        <h3>Execution Result:</h3>
-        <pre>{result}</pre>
-      </div>
-      <div className="result-window">
-        <h3>Logs</h3>
-        <pre>{logs}</pre>
-      </div>
+
+        <div class="alert alert-warning text-center" role="alert">
+          Select your language, write your code, and click Submit!
+        </div>
+      </section>
+
+      <section id="editor">
+        <Editor
+          height="70vh"
+          defaultLanguage={language}
+          value={code}
+          onChange={handleEditorChange}
+        />
+      </section>
+      
+      <section id="result">
+        <div className="result-window">
+          <h3>Execution Result : </h3>
+          <pre>x = 2342{result}</pre>
+        </div>
+        <div className="result-window">
+          <h3>Logs : </h3>
+          <pre>Code executed fine{logs}</pre>
+        </div>
+      </section>
 
     </div>
   );
