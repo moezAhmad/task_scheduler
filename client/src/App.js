b9e969cc9d4e05648860b9a14174eaa67eb1d7fa
Humza Tareen <humzakhawartareen@gmail.com>
2023-05-17 16:17:20 +0500

    IP Adresses of Hotspot

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   const handleSubmit = async () => {
     console.log("Submitting the following code:", code);
     try {
-      const response = await axios.post('http://192.168.137.43:3000/submit-code', {
+      const response = await axios.post('http://192.168.89.233:3000/submit-code', {
         language,
         code,
       });
